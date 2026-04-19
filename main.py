6289568676af8512b8850ef8f5bee2daf7656ee9
PerenNana <paul.nana-nana@eoniq.ai>
2026-04-19 16:06:48 +0200

    langchain_client done

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+
+
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python", 
+    args=["/home/paper/eoniq/langchain/langchain-course/documentation-helper/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from documentation-helper!")
